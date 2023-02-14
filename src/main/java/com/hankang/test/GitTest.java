920a9dcff3487618b44427fb540b5a695719dc9f
hk <hk@java.com>
2023-02-14 17:29:02 +0800

    second commit

--- src/main/java/com/hankang/test/GitTest.java
+++ src/main/java/com/hankang/test/GitTest.java
@@ -2,7 +2,7 @@ package com.hankang.test;
 
 public class GitTest {
     public static void main(String[] args) {
-
         System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
     }
 }
